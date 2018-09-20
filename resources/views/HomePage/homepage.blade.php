11625eb0362d5e16873de9203a311b4d65011daa
SahanBuddhika <sahan.bu.1994@gmail.com>
2018-09-21 04:15:08 +0530

    crete homepage and visible it after login

---
+++ resources/views/HomePage/homepage.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <link rel="stylesheet" href=/css/style.css>
+        <body>
+            
+<h>Welcome To My Page</h>
+
+        </body>
+    
+</html>
